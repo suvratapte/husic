6c82308c7f5387903a7de9a835ddd9e435d58436
Suvrat Apte <suvratapte@gmail.com>
2021-11-30 23:07:02 +0100

    Add support for BPM

--- Main.hs
+++ Main.hs
@@ -12,6 +12,7 @@ type Samples = Float
 type Hz = Float
 type Pulse = Float
 type Semitones = Float
+type Beats = Float
 
 outputFilePath :: FilePath
 outputFilePath = "output.bin"
@@ -25,20 +26,26 @@ volume = 0.2
 pitchStandard :: Hz
 pitchStandard = 440.0 -- pitch standard for the note `A`
 
+bpm :: Beats
+bpm = 120.0
+
+beatDuration :: Seconds
+beatDuration = 60.0 / bpm
+
 semitone :: Semitones -> Hz
 semitone n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
 
-sa  = semitone 0
-re  = semitone 2
-ga  = semitone 4
-ma  = semitone 5
-pa  = semitone 7
-dha = semitone 9
-ni  = semitone 11
-sa' = semitone 12
+sa  = note 0
+re  = note 2
+ga  = note 4
+ma  = note 5
+pa  = note 7
+dha = note 9
+ni  = note 11
+sa' = note 12
 
-note :: Semitones -> Seconds -> [Pulse]
-note n duration = freq (semitone n) duration
+note :: Semitones -> Beats -> [Pulse]
+note n beats = freq (semitone n) (beats * beatDuration)
 
 freq :: Hz -> Seconds -> [Pulse]
 freq hz duration =
@@ -64,19 +71,17 @@ freq hz duration =
     output = map sin $ map (* step) [0.0 .. sampleRate * duration]
 
 wave :: [Pulse]
-wave = concat [ freq sa duration
-              , freq re duration
-              , freq ga duration
-              , freq ma duration
-              , freq pa duration
-              , freq dha duration
-              , freq ni duration
-              , freq sa' duration
+wave = concat [ sa beats
+              , re beats
+              , ga beats
+              , ma beats
+              , pa beats
+              , dha beats
+              , ni beats
+              , sa' beats
               ]
   where
-    duration = 0.5
-
-
+    beats = 1
 
 save :: IO ()
 save = B.writeFile outputFilePath $ B.toLazyByteString $ fold $ map B.floatLE wave
